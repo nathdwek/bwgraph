a72cc72b99c067766335412bfd58bef6d2e0a474
nath@Home <nathdwek@ulb.ac.be>
2015-12-18 23:22:30 +0100

    cleaner

---
+++ bwstore.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+import json
+from time import time
+from sys import stdin, argv
+
+
+def add_test(results, dbFile):
+    try:
+        db = open(dbFile, 'r')
+        existingData = json.load(db)
+        db.close()
+    except FileNotFoundError:
+        existingData = []
+    existingData.append(results)
+    db = open(dbFile, 'w')
+    json.dump(existingData, db, indent=2)
+    db.write('\n')
+    db.close()
+
+
+def load_test():
+    result = json.load(stdin)
+    if "timestamp" not in result["start"]:
+        result["start"]["timestamp"] = {"timesecs": round(time())}
+    return result
+
+if __name__ == "__main__":
+    add_test(load_test(), argv[1])
---
+++ graph.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+
+import json
+import matplotlib.pyplot as plt
+from sys import argv
+
+
+def parse_db(dbFile):
+    db = open(dbFile, 'r')
+    data = json.load(db)
+    db.close()
+    bws = []
+    times = []
+    for result in data:
+        times.append(result["start"]["timestamp"]["timesecs"])
+        try:
+            bws.append(result["end"]["sum_sent"]["bits_per_second"])
+        except:
+            bws.append(0)
+    plt.plot(times, bws)
+    plt.show()
+    return times, bws
+
+
+if __name__ == "__main__":
+    print(parse_db(argv[1]))
